8f25f43bf8575b5acb5cf916f9f52a87d6299c7f
Hoang Nguyen <hoangisnot@gmail.com>
2023-03-15 14:40:47 -0700

    import store into index.js
    add in action creator in  store -> index.js line 32 to update addSong(state, action)

--- my-app/src/store/index.js
+++ my-app/src/store/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import {useSelector} from 'react-redux'
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
 const songsSlice = createSlice ({
   name: 'song',
   initialState: [],
   reducers: {
+    //'song' + '/' + 'addSong' = 'song/addSong'
+    // this action will perform for us by addSong
     addSong(state, action) {
+      // this state is not the big state object
+      // it is a piece of state that get manager by this reducers
       state.push(action.payload);
     },
+    //'song' + '/' + 'RemoveSong' = 'song/RemoveSong'
     removeSong(state, action) {
-      // 
+     // 
     }
   }
 })
@@ -21,14 +24,12 @@ const store = configureStore({
   }
 })
 
+// console.log(songsSlice.actions.addSong());
+// console.log(store)
 const startingState = store.getState();
 console.log(JSON.stringify(startingState));
 
-store.dispatch({
-  type: 'song/addSong',
-  payload: 'New Song!!!'
-});
+store.dispatch(songsSlice.actions.addSong("Some Song!"));
 
 const finalState = store.getState();
 console.log(JSON.stringify(finalState));
-
